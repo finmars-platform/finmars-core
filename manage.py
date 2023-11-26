1d9414b41258b5314028231c7e8e47f65480e308
dp.sokolov <sokolovdp@gmail.com>
2023-11-26 16:00:08 +0300

    FN-2233 update manage.py

--- manage.py
+++ manage.py
@@ -8,14 +8,14 @@ if __name__ == "__main__":
     sys.setrecursionlimit(10000)
 
     try:
-        print(f"Django manage.py: PYTHONPATH={sys.path}")
+
         from django.core.management import execute_from_command_line
 
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            f"Couldn't import Django. Are you sure it's installed and "
+            f"available on your PYTHONPATH={sys.path} environment variable?"
+            f" Did you forget to activate a virtual environment?"
         ) from exc
 
     execute_from_command_line(sys.argv)
