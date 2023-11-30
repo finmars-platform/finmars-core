5c3d8a0c32045db865cf66d16cb9649cee6de8f8
dp.sokolov <sokolovdp@gmail.com>
2023-11-30 21:47:34 +0300

    FN-2089 refact utils.py

--- poms/reports/utils.py
+++ poms/reports/utils.py
@@ -2,121 +2,129 @@ import hashlib
 import json
 import logging
 
-_l = logging.getLogger('poms.reports')
+_l = logging.getLogger("poms.reports")
 
 
 def generate_report_unique_hash(app, action, data, master_user, member):
     _data = data.copy()
 
-    result = ''
+    report_options = {
+        "master_user": master_user.id,
+        "member": member.id,
+    }
 
-    report_options = {}
+    if "begin_date" in _data:
+        report_options["begin_date"] = _data["begin_date"]
 
-    report_options['master_user'] = master_user.id
-    report_options['member'] = member.id
+    if "date_field" in _data:
+        report_options["date_field"] = _data["date_field"]
 
-    if 'begin_date' in _data:
-        report_options['begin_date'] = _data['begin_date']
+    if "end_date" in _data:
+        report_options["end_date"] = _data["end_date"]
 
-    if 'date_field' in _data:
-        report_options['date_field'] = _data['date_field']
+    if "report_date" in _data:
+        report_options["report_date"] = _data["report_date"]
 
-    if 'end_date' in _data:
-        report_options['end_date'] = _data['end_date']
+    if "pl_first_date" in _data:
+        report_options["pl_first_date"] = _data["pl_first_date"]
 
-    if 'report_date' in _data:
-        report_options['report_date'] = _data['report_date']
+    if "report_currency" in _data:
+        report_options["report_currency"] = _data["report_currency"]
 
-    if 'pl_first_date' in _data:
-        report_options['pl_first_date'] = _data['pl_first_date']
+    if "pricing_policy" in _data:
+        report_options["pricing_policy"] = _data["pricing_policy"]
 
-    if 'report_currency' in _data:
-        report_options['report_currency'] = _data['report_currency']
+    if "report_type" in _data:
+        report_options["report_type"] = _data["report_type"]
 
-    if 'pricing_policy' in _data:
-        report_options['pricing_policy'] = _data['pricing_policy']
+    if "account_mode" in _data:
+        report_options["account_mode"] = _data["account_mode"]
 
-    if 'report_type' in _data:
-        report_options['report_type'] = _data['report_type']
+    if "portfolio_mode" in _data:
+        report_options["portfolio_mode"] = _data["portfolio_mode"]
 
-    if 'account_mode' in _data:
-        report_options['account_mode'] = _data['account_mode']
+    if "strategy1_mode" in _data:
+        report_options["strategy1_mode"] = _data["strategy1_mode"]
 
-    if 'portfolio_mode' in _data:
-        report_options['portfolio_mode'] = _data['portfolio_mode']
+    if "strategy2_mode" in _data:
+        report_options["strategy2_mode"] = _data["strategy2_mode"]
 
-    if 'strategy1_mode' in _data:
-        report_options['strategy1_mode'] = _data['strategy1_mode']
+    if "strategy3_mode" in _data:
+        report_options["strategy3_mode"] = _data["strategy3_mode"]
 
-    if 'strategy2_mode' in _data:
-        report_options['strategy2_mode'] = _data['strategy2_mode']
+    if "allocation_mode" in _data:
+        report_options["allocation_mode"] = _data["allocation_mode"]
 
-    if 'strategy3_mode' in _data:
-        report_options['strategy3_mode'] = _data['strategy3_mode']
+    if "custom_fields_to_calculate" in _data:
+        report_options["custom_fields_to_calculate"] = _data[
+            "custom_fields_to_calculate"
+        ]
 
-    if 'allocation_mode' in _data:
-        report_options['allocation_mode'] = _data['allocation_mode']
+    if "complex_transaction_statuses_filter" in _data:
+        report_options["complex_transaction_statuses_filter"] = _data[
+            "complex_transaction_statuses_filter"
+        ]
 
-    if 'custom_fields_to_calculate' in _data:
-        report_options['custom_fields_to_calculate'] = _data['custom_fields_to_calculate']
+    if "cost_method" in _data:
+        report_options["cost_method"] = _data["cost_method"]
 
-    if 'complex_transaction_statuses_filter' in _data:
-        report_options['complex_transaction_statuses_filter'] = _data['complex_transaction_statuses_filter']
+    if "show_balance_exposure_details" in _data:
+        report_options["show_balance_exposure_details"] = _data[
+            "show_balance_exposure_details"
+        ]
 
-    if 'cost_method' in _data:
-        report_options['cost_method'] = _data['cost_method']
+    if "show_transaction_details" in _data:
+        report_options["show_transaction_details"] = _data["show_transaction_details"]
 
-    if 'show_balance_exposure_details' in _data:
-        report_options['show_balance_exposure_details'] = _data['show_balance_exposure_details']
+    if "approach_multiplier" in _data:
+        report_options["approach_multiplier"] = _data["approach_multiplier"]
 
-    if 'show_transaction_details' in _data:
-        report_options['show_transaction_details'] = _data['show_transaction_details']
+    if "portfolios" in _data:
+        report_options["portfolios"] = _data["portfolios"]
 
-    if 'approach_multiplier' in _data:
-        report_options['approach_multiplier'] = _data['approach_multiplier']
+    if "accounts" in _data:
+        report_options["accounts"] = _data["accounts"]
 
-    if 'portfolios' in _data:
-        report_options['portfolios'] = _data['portfolios']
+    if "strategies1" in _data:
+        report_options["strategies1"] = _data["strategies1"]
 
-    if 'accounts' in _data:
-        report_options['accounts'] = _data['accounts']
+    if "strategies2" in _data:
+        report_options["strategies2"] = _data["strategies2"]
 
-    if 'strategies1' in _data:
-        report_options['strategies1'] = _data['strategies1']
-
-    if 'strategies2' in _data:
-        report_options['strategies2'] = _data['strategies2']
-
-    if 'strategies3' in _data:
-        report_options['strategies3'] = _data['strategies3']
+    if "strategies3" in _data:
+        report_options["strategies3"] = _data["strategies3"]
 
     # Performance report field
 
-    if 'calculation_type' in _data:
-        report_options['calculation_type'] = _data['calculation_type']
+    if "calculation_type" in _data:
+        report_options["calculation_type"] = _data["calculation_type"]
 
-    if 'segmentation_type' in _data:
-        report_options['segmentation_type'] = _data['segmentation_type']
+    if "segmentation_type" in _data:
+        report_options["segmentation_type"] = _data["segmentation_type"]
 
-    if 'calculation_type' in _data:
-        report_options['calculation_type'] = _data['calculation_type']
+    if "calculation_type" in _data:
+        report_options["calculation_type"] = _data["calculation_type"]
 
-    if 'registers' in _data:
-        report_options['registers'] = _data['registers']
+    if "registers" in _data:
+        report_options["registers"] = _data["registers"]
 
-    if 'bundle' in _data:
-        report_options['bundle'] = _data['bundle']
+    if "bundle" in _data:
+        report_options["bundle"] = _data["bundle"]
 
-    if 'depth_level' in _data:
-        report_options['depth_level'] = _data['depth_level']
+    if "depth_level" in _data:
+        report_options["depth_level"] = _data["depth_level"]
 
-    if 'filters' in _data:
-        report_options['filters'] = str(_data['filters'])
+    if "filters" in _data:
+        report_options["filters"] = str(_data["filters"])
 
-    if 'expression_iterations_count' in _data:
-        report_options['expression_iterations_count'] = _data['expression_iterations_count']
+    if "expression_iterations_count" in _data:
+        report_options["expression_iterations_count"] = _data[
+            "expression_iterations_count"
+        ]
 
-    result = app + '_' + action + '_' + str(master_user.id) + '_' + str(member.id) + '_' + hashlib.md5(
-        json.dumps(report_options, sort_keys=True).encode('utf-8')).hexdigest()
-
-    return result
+    return (
+        f"{app}_{action}_{str(master_user.id)}_{str(member.id)}_"
+        + hashlib.md5(
+            json.dumps(report_options, sort_keys=True).encode("utf-8")
+        ).hexdigest()
+    )
