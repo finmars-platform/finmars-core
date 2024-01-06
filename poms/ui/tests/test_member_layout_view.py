8fd91fbdba1008eba0a609a2f85131118cb8dd25
dp.sokolov <sokolovdp@gmail.com>
2024-01-06 22:04:46 +0300

    FN-1600 add ping test

--- poms/ui/tests/test_member_layout_view.py
+++ poms/ui/tests/test_member_layout_view.py
@@ -15,5 +15,21 @@ class MemberLayoutViewSetTest(BaseTestCase):
         self.assertEqual(response.status_code, 200, response.content)
 
         response_json = response.json()
+        self.assertEqual(response_json["count"], 1)
+        self.assertIsNone(response_json["next"])
+        self.assertIsNone(response_json["previous"])
 
-        print(response_json)
+        layout_data = response_json["results"][0]
+        self.assertEqual(layout_data["name"], "default")
+        self.assertEqual(layout_data["configuration_code"], "local.poms.space00000")
+        self.assertTrue(layout_data["is_default"])
+
+    def test__ping(self):
+        layout = MemberLayout.objects.first()
+        response = self.client.get(path=f"{self.url}{layout.id}/ping/")
+        self.assertEqual(response.status_code, 200, response.content)
+
+        response_json = response.json()
+        self.assertEqual(response_json["id"], layout.id)
+        self.assertEqual(response_json["modified"], layout.modified)
+        self.assertEqual(response_json["is_default"], layout.is_default)
