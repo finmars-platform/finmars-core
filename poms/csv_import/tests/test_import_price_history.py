4349e895929fbae60ab361a40725223c04807a01
dp.sokolov <sokolovdp@gmail.com>
2024-01-19 11:33:46 +0100

    PLAT-284 update test_import_price_history.py

--- poms/csv_import/tests/test_import_price_history.py
+++ poms/csv_import/tests/test_import_price_history.py
@@ -41,20 +41,29 @@ class ImportPriceHistoryTest(BaseTestCase):
         )
         return CsvImportScheme.objects.create(**schema_data)
 
+    @mock.patch("poms.csv_import.views.simple_import.apply_async")
     @mock.patch("poms.csv_import.serializers.storage")
-    def test(self, mock_storage):
+    def test(self, mock_storage, mock_async):
         file_content = SimpleUploadedFile(FILE_NAME, FILE_CONTENT)
         mock_storage.return_value = self.storage
         request_data = {"file": file_content, "scheme": self.scheme_20.id}
+
         response = self.client.post(
             path=self.url,
             data=request_data,
             format="multipart",
         )
         self.assertEqual(response.status_code, 200, response.content)
+
+        mock_async.assert_called_with(
+            kwargs={"task_id": 1},
+            queue="backend-background-queue",
+        )
+
         response_json = response.json()
         self.assertIn("task_id", response_json)
         self.assertIn("task_status", response_json)
+        self.assertEqual(response_json["task_id"], 1)
         self.assertEqual(response_json["task_status"], CeleryTask.STATUS_INIT)
 
         celery_task = CeleryTask.objects.get(pk=response_json["task_id"])
