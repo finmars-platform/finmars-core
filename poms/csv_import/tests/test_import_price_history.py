c59ebd8206ee5fef6bd487d3371dd9f0e9f889c3
dp.sokolov <sokolovdp@gmail.com>
2024-01-19 12:44:30 +0100

    PLAT-284 fix linters, update test_import_price_history.py

--- poms/csv_import/tests/test_import_price_history.py
+++ poms/csv_import/tests/test_import_price_history.py
@@ -8,6 +8,8 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 from poms.celery_tasks.models import CeleryTask
 from poms.common.common_base_test import BaseTestCase
 from poms.csv_import.models import CsvImportScheme
+from poms.csv_import.handlers import SimpleImportProcess
+from poms.csv_import.tasks import simple_import
 from poms.csv_import.tests.common_test_data import PRICE_HISTORY, SCHEMA_20
 
 API_URL = f"/{settings.BASE_API_URL}/api/v1/import/csv/"
@@ -41,9 +43,24 @@ class ImportPriceHistoryTest(BaseTestCase):
         )
         return CsvImportScheme.objects.create(**schema_data)
 
+    def create_task(self):
+        options_object = {
+            "file_path": FILE_NAME,
+            "filename": FILE_NAME,
+            "scheme_id": self.scheme_20.id,
+            "execution_context": None,
+        }
+        return CeleryTask.objects.create(
+            master_user=self.master_user,
+            member=self.member,
+            options_object=options_object,
+            verbose_name="Simple Import",
+            type="simple_import",
+        )
+
     @mock.patch("poms.csv_import.views.simple_import.apply_async")
     @mock.patch("poms.csv_import.serializers.storage")
-    def test(self, mock_storage, mock_async):
+    def test_view(self, mock_storage, mock_async):
         file_content = SimpleUploadedFile(FILE_NAME, FILE_CONTENT)
         mock_storage.return_value = self.storage
         request_data = {"file": file_content, "scheme": self.scheme_20.id}
@@ -55,15 +72,11 @@ class ImportPriceHistoryTest(BaseTestCase):
         )
         self.assertEqual(response.status_code, 200, response.content)
 
-        mock_async.assert_called_with(
-            kwargs={"task_id": 1},
-            queue="backend-background-queue",
-        )
+        mock_async.assert_called_once()
 
         response_json = response.json()
         self.assertIn("task_id", response_json)
         self.assertIn("task_status", response_json)
-        self.assertEqual(response_json["task_id"], 1)
         self.assertEqual(response_json["task_status"], CeleryTask.STATUS_INIT)
 
         celery_task = CeleryTask.objects.get(pk=response_json["task_id"])
@@ -71,3 +84,17 @@ class ImportPriceHistoryTest(BaseTestCase):
 
         self.assertEqual(options["filename"], FILE_NAME)
         self.assertEqual(options["scheme_id"], self.scheme_20.id)
+
+    @mock.patch("poms.csv_import.handlers.SimpleImportProcess")
+    def test_simple_import_task(self, mock_import_process):
+        mock_instance = mock_import_process.return_value
+
+        task = self.create_task()
+        simple_import(task_id=task.id)
+
+        mock_import_process.assert_called()
+
+        task.refresh_from_db()
+        self.assertEqual(task.status, CeleryTask.STATUS_PENDING)
+
+        print(f"task.progress_object={task.progress}")
