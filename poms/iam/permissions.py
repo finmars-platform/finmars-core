a185af55867b405f07badc34661a103e73843367
dp.sokolov <sokolovdp@gmail.com>
2024-02-25 15:00:42 +0300

    PLAT-440 fix FinmarsAccessPolicy in permissions.py

--- poms/iam/permissions.py
+++ poms/iam/permissions.py
@@ -1,16 +1,17 @@
-
 import logging
+
+from rest_framework.exceptions import PermissionDenied
 
 from poms.iam.access_policy import AccessPolicy
 from poms.iam.utils import get_statements
 
-_l = logging.getLogger('poms.iam')
+_l = logging.getLogger("poms.iam")
+
 
 class FinmarsAccessPolicy(AccessPolicy):
+    def get_policy_statements(self, request, view=None) -> list:
+        if hasattr(request.user, "member") and request.user.member:
+            return get_statements(member=request.user.member)
 
-    def get_policy_statements(self, request, view=None):
-
-        return get_statements(member=request.user.member)
-
-
-
+        else:
+            raise PermissionDenied(f"User {request.user.username} has no member")
