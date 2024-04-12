3e5d6537454146719cc4c2ca086e5ba955e02c4f
dp.sokolov <sokolovdp@gmail.com>
2024-04-12 18:34:33 +0300

    PLAT-564 refac

--- poms/integrations/database_client.py
+++ poms/integrations/database_client.py
@@ -3,6 +3,7 @@ import traceback
 
 from django.conf import settings
 
+from poms.common.exceptions import FinmarsBaseException
 from poms.common.http_client import HttpClient, HttpClientError
 from poms.integrations.monad import Monad, MonadStatus
 from poms.integrations.serializers import CallBackDataDictRequestSerializer
@@ -13,7 +14,7 @@ log = "DatabaseClient"
 # TODO REALM_REFACTOR: szhitenev change to realm_code callback
 
 
-def get_backend_callback_url():
+def get_backend_callback_url() -> dict:
     from poms.users.models import MasterUser
 
     master_user = MasterUser.objects.all().first()
@@ -78,9 +79,14 @@ class DatabaseService:
             )
         except HttpClientError as e:
             return Monad(status=MonadStatus.ERROR, message=repr(e))
+
         except Exception as e:
-            _l.error(f"{log}.get_monad unexpected {repr(e)} {traceback.format_exc()}")
-            raise
+            err_msg = f"{log} unexpected {repr(e)}"
+            _l.error(f"{err_msg}  trace {traceback.format_exc()}")
+            raise FinmarsBaseException(
+                message=err_msg,
+                error_key="finmars_database_error",
+            ) from e
 
         serializer = CallBackDataDictMonadSerializer(data=response_json)
         return (
