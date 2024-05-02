e054ffa49435b3911ee6fda14bfef2e870e70534
dp.sokolov <sokolovdp@gmail.com>
2024-05-02 23:14:20 +0200

    PLAT-653 optimize tests

--- poms/explorer/tests/test_explorer_view.py
+++ poms/explorer/tests/test_explorer_view.py
@@ -1,23 +1,23 @@
-from unittest import mock
 from datetime import datetime
+from unittest import mock
 
 from poms.common.common_base_test import BaseTestCase
-
 from poms.common.storage import FinmarsS3Storage
+
+storage_mock = mock.MagicMock(spec=FinmarsS3Storage)
 
 
 class ExplorerViewSetTest(BaseTestCase):
     def setUp(self):
         super().setUp()
         self.init_test_case()
+
         self.realm_code = "realm00000"
         self.space_code = "space00000"
         self.url = f"/{self.realm_code}/{self.space_code}/api/v1/explorer/explorer/"
 
-    @mock.patch("poms.explorer.views.storage")
-    def test__no_path(self, storage):
-        storage.return_value = mock.MagicMock(spec=FinmarsS3Storage)
-
+    @mock.patch("poms.explorer.views.storage", return_value=storage_mock)
+    def test__no_path(self, _):
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 400)
 
@@ -26,10 +26,8 @@ class ExplorerViewSetTest(BaseTestCase):
         ("2", "/test"),
         ("3", "/test/"),
     )
-    @mock.patch("poms.explorer.views.storage")
-    def test__path_ends_with_slash(self, path, storage):
-        storage.return_value = mock.MagicMock(spec=FinmarsS3Storage)
-
+    @mock.patch("poms.explorer.views.storage", return_value=storage_mock)
+    def test__path_ends_with_slash(self, path, _):
         response = self.client.get(self.url, {"path": path})
         self.assertEqual(response.status_code, 400)
 
@@ -37,9 +35,8 @@ class ExplorerViewSetTest(BaseTestCase):
         ("test", "test"),
         ("test_test", "test/test"),
     )
-    @mock.patch("poms.explorer.views.storage")
+    @mock.patch("poms.explorer.views.storage", return_value=storage_mock)
     def test__with_empty_path(self, path, storage):
-        storage.return_value = mock.MagicMock(spec=FinmarsS3Storage)
         storage.listdir.return_value = [], []
 
         response = self.client.get(self.url, {"path": path})
@@ -53,14 +50,14 @@ class ExplorerViewSetTest(BaseTestCase):
         self.assertEqual(response_data["path"], f"{self.space_code}/{path}/")
         self.assertEqual(response_data["results"], [])
 
-    @mock.patch("poms.explorer.views.mimetypes.guess_type")
-    @mock.patch("poms.explorer.views.storage")
-    def test__path(self, storage, mock_guess_type):
-        mock_guess_type.return_value = "text/plain", "utf-8"
-        storage.return_value = mock.MagicMock(spec=FinmarsS3Storage)
+    @mock.patch(
+        "poms.explorer.views.mimetypes.guess_type", return_value=("text/plain", "utf-8")
+    )
+    @mock.patch("poms.explorer.views.storage", return_value=storage_mock)
+    def test__path(self, storage, _):
         directories = ["first", "second"]
         files = ["file.csv", "file.txt", "file.json"]
-        path = self.random_string(10)
+        path = self.random_string()
         storage.listdir.return_value = directories, files
         storage.get_created_time.return_value = datetime.now()
         storage.get_modified_time.return_value = datetime.now()
