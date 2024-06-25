a6e6a0c7ab14bbaea014dbe0576ec0c8e4194222
dp.sokolov <sokolovdp@gmail.com>
2024-06-25 20:23:34 +0300

    PLAT-655 add patch test

--- poms/instruments/tests/views/test_files.py
+++ poms/instruments/tests/views/test_files.py
@@ -149,3 +149,24 @@ class FinmarsFileViewSetTest(BaseTestCase):
         self.assertEqual(response.status_code, 200, response.content)
         response_json = response.json()
         self.assertEqual(response_json["path"], file_data["path"])
+
+    def test__patch(self):
+        file_data = dict(
+            name=f"{self.random_string(12)}.pdf",
+            path=f"/{self.random_string()}/{self.random_string(5)}/",
+            size=self.random_int(10, 1000000000),
+        )
+        response = self.client.post(path=self.url, data=file_data, format="json")
+        self.assertEqual(response.status_code, 201, response.content)
+        response_json = response.json()
+        file_id = response_json["id"]
+        self.assertEqual(response_json["path"], file_data["path"])
+
+        patch_data = {"path": "/root/"}
+        response = self.client.patch(
+            path=f"{self.url}{file_id}/", data=patch_data, format="json"
+        )
+        self.assertEqual(response.status_code, 200, response.content)
+
+        file = FinmarsFile.objects.get(id=file_id)
+        self.assertEqual(file.path, "/root/")
