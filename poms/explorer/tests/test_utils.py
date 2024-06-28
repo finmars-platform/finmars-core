b75a8ebf0feb94a66e49560bbf131cf94f9695ca
dp.sokolov <sokolovdp@gmail.com>
2024-06-28 19:24:31 +0300

    PLAT-655 add util tests

--- poms/explorer/tests/test_utils.py
+++ poms/explorer/tests/test_utils.py
@@ -167,3 +167,23 @@ class SyncFileInDatabaseTest(BaseTestCase):
         self.assertIsNotNone(file)
         self.assertEqual(file.size, size)
         self.assertEqual(file.path, "/test/super")
+        self.assertEqual(file.extension, "pdf")
+
+    def test__update_existing_object(self):
+        filepath = "/test/next/file.pdf"
+        old_size = self.random_int(10, 100000000)
+        self.storage.size.return_value = old_size
+
+        sync_file_in_database(self.storage, filepath)
+
+        file = FinmarsFile.objects.filter(name="file.pdf").first()
+        self.assertEqual(file.size, old_size)
+
+        # test that new size will be used in existing File
+        new_size = self.random_int(100000, 100000000000)
+        self.storage.size.return_value = new_size
+
+        sync_file_in_database(self.storage, filepath)
+
+        file = FinmarsFile.objects.filter(name="file.pdf").first()
+        self.assertEqual(file.size, new_size)
