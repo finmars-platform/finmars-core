a032fbdebf129e1cceda7e4b5340fa7b63e66050
dp.sokolov <sokolovdp@gmail.com>
2024-07-11 21:55:11 +0300

    PLAT-657 update admin.py

--- poms/explorer/admin.py
+++ poms/explorer/admin.py
@@ -1,8 +1,19 @@
-from poms.common.admin import AbstractModelAdmin
-from poms.explorer.models import FinmarsFile
 from django.contrib import admin
+
+from poms.common.admin import AbstractModelAdmin
+from poms.explorer.models import FinmarsDirectory, FinmarsFile, ShareAccessRecord
 
 
 @admin.register(FinmarsFile)
 class FinmarsFileAdmin(AbstractModelAdmin):
     model = FinmarsFile
+
+
+@admin.register(FinmarsDirectory)
+class FinmarsDirAdmin(AbstractModelAdmin):
+    model = FinmarsDirectory
+
+
+@admin.register(ShareAccessRecord)
+class FinmarsDirAdmin(AbstractModelAdmin):
+    model = ShareAccessRecord
