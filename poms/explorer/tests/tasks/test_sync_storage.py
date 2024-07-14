8bd256cd5740add69a449bf3d622947839a22c75
dp.sokolov <sokolovdp@gmail.com>
2024-07-14 21:27:11 +0300

    PLAT-657 - fix tests

--- poms/explorer/tests/tasks/test_sync_storage.py
+++ poms/explorer/tests/tasks/test_sync_storage.py
@@ -1,9 +1,9 @@
 from unittest import mock
 
-from poms.celery_tasks.models import CeleryTask
+# from poms.celery_tasks.models import CeleryTask
 from poms.common.common_base_test import BaseTestCase
 from poms.common.storage import FinmarsS3Storage
-from poms.explorer.models import FinmarsFile
+from poms.explorer.models import FinmarsDirectory, FinmarsFile
 from poms.explorer.utils import (
     sync_file_in_database,
     sync_storage_objects,
@@ -20,37 +20,40 @@ class SyncFileInDatabaseTest(BaseTestCase):
         )
         self.storage = self.storage_patch.start()
         self.addCleanup(self.storage_patch.stop)
+        self.directory = FinmarsDirectory.objects.create(path="/test/next")
 
     def test__created_new_object(self):
-        filepath = "/test/super/file.pdf"
+        name = "file.doc"
+        filepath = f"{self.directory.path}/{name}"
         size = self.random_int(1000, 1000000)
         self.storage.size.return_value = size
 
-        sync_file_in_database(self.storage, filepath)
+        sync_file_in_database(self.storage, filepath, self.directory)
 
-        file = FinmarsFile.objects.filter(name="file.pdf").first()
+        file = FinmarsFile.objects.filter(name=name).first()
         self.assertIsNotNone(file)
         self.assertEqual(file.size, size)
-        self.assertEqual(file.path, "/test/super")
-        self.assertEqual(file.extension, "pdf")
+        self.assertEqual(file.fullpath, filepath)
+        self.assertEqual(file.extension, "doc")
 
     def test__update_existing_object(self):
-        filepath = "/test/next/file.pdf"
+        name = "file.doc"
+        filepath = f"{self.directory.path}/{name}"
         old_size = self.random_int(10, 100000000)
         self.storage.size.return_value = old_size
 
-        sync_file_in_database(self.storage, filepath)
+        sync_file_in_database(self.storage, filepath, self.directory)
 
-        file = FinmarsFile.objects.filter(name="file.pdf").first()
+        file = FinmarsFile.objects.filter(name=name).first()
         self.assertEqual(file.size, old_size)
 
         # test that new size will be used in existing File
         new_size = self.random_int(100000, 100000000000)
         self.storage.size.return_value = new_size
 
-        sync_file_in_database(self.storage, filepath)
+        sync_file_in_database(self.storage, filepath, self.directory)
 
-        file = FinmarsFile.objects.filter(name="file.pdf").first()
+        file = FinmarsFile.objects.filter(name=name).first()
         self.assertEqual(file.size, new_size)
 
 
@@ -64,16 +67,19 @@ class SyncFilesTest(BaseTestCase):
         )
         self.storage = self.storage_patch.start()
         self.addCleanup(self.storage_patch.stop)
+        self.directory = FinmarsDirectory.objects.create(path="/root")
 
     def test__files_created(self):
         # Mock the listdir return values
-        f1 = "/test/next/file_1.doc"
-        f2 = "/test/next/file_2.zip"
+        f1 = "file.xls"
+        f2 = "file.zip"
+        filepath_1 = f"{self.directory.path}/{f1}"
+        filepath_2 = f"{self.directory.path}/{f2}"
         size = self.random_int(10000, 100000000)
-        self.storage.listdir.return_value = ([], [f1, f2])
+        self.storage.listdir.return_value = ([], [filepath_1, filepath_2])
         self.storage.size.return_value = size
 
-        sync_storage_objects(self.storage, "/test/next")
+        sync_storage_objects(self.storage, self.directory)
 
         files = FinmarsFile.objects.all()
 
