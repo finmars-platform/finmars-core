be8ed2425e5b2c6abdb9277e37eeca403e95a863
dp.sokolov <sokolovdp@gmail.com>
2024-07-17 22:35:08 +0300

    PLAT-657  update policy_handlers.py & tests

--- poms/explorer/policy_handlers.py
+++ poms/explorer/policy_handlers.py
@@ -1,12 +1,19 @@
 from copy import deepcopy
-from typing import Union
+from typing import List, Union
+import logging
 
 from django.conf import settings
+from django.core.cache import cache
+from django.db.models import Q
 
 from poms.configuration.utils import get_default_configuration_code
 from poms.explorer.models import FULL, READ, FinmarsDirectory, FinmarsFile
 from poms.iam.models import AccessPolicy
 from poms.users.models import Member
+
+StorageObject = Union[FinmarsFile, FinmarsDirectory]
+
+_l = logging.getLogger("poms.explorer")
 
 RESOURCE = f"frn:{settings.SERVICE_NAME}:explorer:{{resource}}"
 
@@ -26,17 +33,14 @@ READ_ACCESS_POLICY = {
 }
 
 
-def validate_obj_access(obj: Union[FinmarsFile, FinmarsDirectory], access: str):
-    if not (isinstance(obj, FinmarsFile) or isinstance(obj, FinmarsDirectory)):
+def validate_obj_access(obj: StorageObject, access: str):
+    if not isinstance(obj, StorageObject):
         raise ValueError("Object must be a FinmarsFile or FinmarsDirectory")
     if access not in {READ, FULL}:
         raise ValueError(f"Access must be either '{READ}' or '{FULL}'")
 
 
-def create_policy(
-    obj: Union[FinmarsFile, FinmarsDirectory],
-    access: str = READ,
-) -> dict:
+def create_policy(obj: StorageObject, access: str = READ) -> dict:
     """
     A function that creates a policy dict based on the type of
     object (file or directory) and the access level.
@@ -57,17 +61,15 @@ def create_policy(
     return policy
 
 
-def upsert_storage_obj_access_policy(
-    obj: Union[FinmarsFile, FinmarsDirectory], owner: Member, access: str
-) -> AccessPolicy:
+def upsert_storage_obj_access_policy(obj: StorageObject, owner: Member, access: str):
     validate_obj_access(obj, access)
 
     configuration_code = get_default_configuration_code()
-    user_code = obj.policy_user_code()
+    user_code = f"{obj.policy_user_code()}-{access}"
     name = obj.resource
     policy = create_policy(obj, access)
     description = f"{name} : {access} access policy"
-    access_policy, created = AccessPolicy.objects.update_or_create(
+    AccessPolicy.objects.update_or_create(
         user_code=user_code,
         owner=owner,
         defaults={
@@ -78,21 +80,63 @@ def upsert_storage_obj_access_policy(
         },
     )
 
-    return access_policy
 
-
-def create_default_access_policy(
-    obj: Union[FinmarsFile, FinmarsDirectory]
-) -> AccessPolicy:
+def create_default_storage_access_policies():
     owner = Member.objects.get(username="finmars_bot")
-    return upsert_storage_obj_access_policy(obj, owner, FULL)
+    for directory in FinmarsDirectory.objects.all().iterator():
+        upsert_storage_obj_access_policy(directory, owner, FULL)
+        upsert_storage_obj_access_policy(directory, owner, READ)
+
+    for file in FinmarsFile.objects.all().iterator():
+        upsert_storage_obj_access_policy(file, owner, FULL)
+        upsert_storage_obj_access_policy(file, owner, READ)
 
 
-def verify_access_policy(
-    member: Member,
-    obj: Union[FinmarsFile, FinmarsDirectory],
-    access: str,
-):
+def add_user_to_storage_obj_policy(obj: StorageObject, user: Member, access: str):
+    validate_obj_access(obj, access)
+    user_code = f"{obj.policy_user_code()}-{access}"
+
+    try:
+        access_policy = AccessPolicy.objects.filter(user_code=user_code).first()
+    except AccessPolicy.DoesNotExist:
+        _l.error(f"No AccessPolicy for user_code={user_code}")
+        raise
+
+    access_policy.members.add(user)
+
+
+def get_member_explorer_policies(member: Member) -> List[AccessPolicy]:
+    """
+    Get all AccessPolicy objects for the member from cache or db
+    Args:
+        member:
+    Returns:
+        list of AccessPolicy objects
+    """
+
+    cache_key = f"member_access_policies_{member.id}"
+    access_policies = cache.get(cache_key)
+
+    if access_policies is None:
+        access_policies = (
+            AccessPolicy.objects.filter(
+                Q(members=member)
+                | Q(iam_roles__members=member)
+                | Q(iam_groups__members=member)
+            )
+            .filter(
+                user_code__contains=":explorer:",
+            )
+            .distinct()
+        )
+
+        # Cache the result for a specific duration (e.g., 5 minutes)
+        cache.set(cache_key, access_policies, settings.ACCESS_POLICY_CACHE_TTL)
+
+    return access_policies
+
+
+def verify_access_policy(member: Member, obj: StorageObject, access: str):
     # TODO load all 'explorer' policies for the user
     # TODO validate path against policies
     return AccessPolicy.objects.filter(user_code=obj.policy_user_code()).exists()
--- poms/explorer/tests/models/test_access_policy.py
+++ poms/explorer/tests/models/test_access_policy.py
@@ -1,11 +1,7 @@
 from poms.common.common_base_test import BaseTestCase
-from poms.configuration.utils import get_default_configuration_code
-from poms.explorer.models import FinmarsDirectory, FinmarsFile, READ
-from poms.explorer.policy_handlers import (
-    RESOURCE,
-    create_default_access_policy,
-    upsert_storage_obj_access_policy,
-)
+from poms.explorer.models import FULL, READ, FinmarsDirectory, FinmarsFile
+from poms.explorer.policy_handlers import create_default_storage_access_policies
+from poms.iam.models import AccessPolicy
 
 EXPECTED_FULL_POLICY = {
     "Version": "2023-01-01",
@@ -26,137 +22,39 @@ class FileAccessPolicyTest(BaseTestCase):
     def setUp(self):
         super().setUp()
         self.init_test_case()
+        self.obj = self._create_file()
 
     def _create_file(self) -> FinmarsFile:
         extension = self.random_string(3)
         name = f"{self.random_string()}.{extension}"
-        path = (
-            f"/{self.random_string()}/{self.random_string(5)}/{name}/"
-        )
+        path = f"/{self.random_string()}/{self.random_string(5)}/{name}/"
         size = self.random_int()
         return FinmarsFile.objects.create(path=path, size=size)
 
-    def test__file_access_policy_created(self):
-        file = self._create_file()
+    def test__created_file_default_policies(self):
+        create_default_storage_access_policies()
 
-        access_policy = create_default_access_policy(file)
+        for access in [READ, FULL]:
+            user_code = f"{self.obj.policy_user_code()}-{access}"
+            default_policy = AccessPolicy.objects.get(user_code=user_code)
+            self.assertIsNotNone(default_policy)
 
-        self.assertEqual(access_policy.name, file.resource)
-        self.assertEqual(
-            access_policy.configuration_code, get_default_configuration_code()
-        )
-        self.assertEqual(access_policy.owner.username, "finmars_bot")
+    def test__user_added_to_file_access_policy(self):
+        create_default_storage_access_policies()
 
-        expected_user_code = (
-            f"local.poms.space00000:finmars:explorer:file:{file.path}"
-        )
-        self.assertEqual(access_policy.user_code, expected_user_code)
-
-        EXPECTED_FULL_POLICY["Statement"][0]["Resource"] = RESOURCE.format(
-            resource=file.resource
-        )
-        self.assertEqual(
-            access_policy.policy, EXPECTED_FULL_POLICY, msg=access_policy.policy
-        )
-
-    def test__file_access_policy_has_unique_user_code(self):
-        file = self._create_file()
-
-        access_policy_1 = create_default_access_policy(file)
-        access_policy_2 = create_default_access_policy(file)
-        self.assertEqual(access_policy_1.id, access_policy_2.id)
-        self.assertEqual(access_policy_1.user_code, access_policy_2.user_code)
-
-    def test__file_access_policy_updated(self):
-        file = self._create_file()
-
-        access_policy_1 = create_default_access_policy(file)
-
-        access_policy_2 = upsert_storage_obj_access_policy(
-            file, access_policy_1.owner, access=READ
-        )
-        self.assertEqual(access_policy_1.id, access_policy_2.id)
-
-        self.assertEqual(access_policy_2.name, file.resource)
-        self.assertEqual(
-            access_policy_2.configuration_code, get_default_configuration_code()
-        )
-        self.assertEqual(access_policy_2.owner.username, "finmars_bot")
-
-        expected_user_code = (
-            f"local.poms.space00000:finmars:explorer:file:{file.path}"
-        )
-        self.assertEqual(access_policy_2.user_code, expected_user_code)
-
-        EXPECTED_FULL_POLICY["Statement"][0]["Resource"] = RESOURCE.format(
-            resource=file.resource
-        )
-        self.assertEqual(len(access_policy_2.policy["Statement"][0]["Action"]), 1)
+        for access in [READ, FULL]:
+            user_code = f"{self.obj.policy_user_code()}-{access}"
+            default_policy = AccessPolicy.objects.get(user_code=user_code)
+            default_policy.members.add(self.member)
 
 
-class DirectoryAccessPolicyTest(BaseTestCase):
+class DirectoryAccessPolicyTest(FileAccessPolicyTest):
     databases = "__all__"
 
     def setUp(self):
         super().setUp()
-        self.init_test_case()
+        self.obj = self._create_directory()
 
     def _create_directory(self) -> FinmarsDirectory:
         path = f"/{self.random_string()}/{self.random_string(3)}"
         return FinmarsDirectory.objects.create(path=path, parent=None)
-
-    def test__directory_access_policy_created(self):
-        directory = self._create_directory()
-
-        access_policy = create_default_access_policy(directory)
-        self.assertEqual(access_policy.name, directory.resource)
-        self.assertEqual(
-            access_policy.configuration_code, get_default_configuration_code()
-        )
-        self.assertEqual(access_policy.owner.username, "finmars_bot")
-
-        expected_user_code = (
-            f"local.poms.space00000:finmars:explorer:dir:{directory.path}"
-        )
-        self.assertEqual(access_policy.user_code, expected_user_code)
-
-        EXPECTED_FULL_POLICY["Statement"][0]["Resource"] = RESOURCE.format(
-            resource=directory.resource
-        )
-        self.assertEqual(
-            access_policy.policy, EXPECTED_FULL_POLICY, msg=access_policy.policy
-        )
-
-    def test__directory_access_policy_has_unique_user_code(self):
-        directory = self._create_directory()
-
-        access_policy_1 = create_default_access_policy(directory)
-        access_policy_2 = create_default_access_policy(directory)
-        self.assertEqual(access_policy_1.id, access_policy_2.id)
-        self.assertEqual(access_policy_1.user_code, access_policy_2.user_code)
-
-    def test__directory_access_policy_updated(self):
-        directory = self._create_directory()
-
-        access_policy_1 = create_default_access_policy(directory)
-
-        access_policy_2 = upsert_storage_obj_access_policy(
-            directory, access_policy_1.owner, access=READ
-        )
-        self.assertEqual(access_policy_1.id, access_policy_2.id)
-
-        self.assertEqual(access_policy_2.name, directory.resource)
-        self.assertEqual(
-            access_policy_2.configuration_code, get_default_configuration_code()
-        )
-        self.assertEqual(access_policy_2.owner.username, "finmars_bot")
-
-        expected_user_code = (
-            f"local.poms.space00000:finmars:explorer:dir:{directory.path}"
-        )
-        self.assertEqual(access_policy_2.user_code, expected_user_code)
-
-        EXPECTED_FULL_POLICY["Statement"][0]["Resource"] = RESOURCE.format(
-            resource=directory.resource
-        )
-        self.assertEqual(len(access_policy_2.policy["Statement"][0]["Action"]), 1)
