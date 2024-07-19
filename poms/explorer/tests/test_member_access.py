6d0f87e5c5d2d3c6b4f6a360fd6df333127d1df1
dp.sokolov <sokolovdp@gmail.com>
2024-07-19 19:16:05 +0300

    PLAT-657 update tests

--- poms/explorer/tests/test_member_access.py
+++ poms/explorer/tests/test_member_access.py
@@ -15,16 +15,16 @@ class MemberHasAccessTest(BaseTestCase):
         self.file = self._create_file()
         self.dir = self._create_directory()
 
-    def _create_file(self) -> FinmarsFile:
+    def _create_file(self, parent: FinmarsDirectory = None) -> FinmarsFile:
         extension = self.random_string(3)
         name = f"{self.random_string()}.{extension}"
         path = f"/{self.random_string()}/{self.random_string(5)}/{name}/"
         size = self.random_int()
-        return FinmarsFile.objects.create(path=path, size=size)
+        return FinmarsFile.objects.create(path=path, size=size, parent=parent)
 
-    def _create_directory(self) -> FinmarsDirectory:
+    def _create_directory(self, parent: FinmarsDirectory = None) -> FinmarsDirectory:
         path = f"/{self.random_string()}/{self.random_string(3)}"
-        return FinmarsDirectory.objects.create(path=path, parent=None)
+        return FinmarsDirectory.objects.create(path=path, parent=parent)
 
     @BaseTestCase.cases(
         ("read", AccessLevel.READ),
@@ -41,3 +41,67 @@ class MemberHasAccessTest(BaseTestCase):
     def test__access_to_dir(self, access):
         get_or_create_storage_access_policy(self.dir, self.member, access)
         self.assertTrue(member_has_access(self.dir, self.member, access))
+
+    @BaseTestCase.cases(
+        ("read", AccessLevel.READ),
+        ("full", AccessLevel.FULL),
+    )
+    def test__access_to_dir(self, access):
+        get_or_create_storage_access_policy(self.dir, self.member, access)
+        self.assertTrue(member_has_access(self.dir, self.member, access))
+
+    def create_dir_tree(self):
+        kwargs = dict(path="/root")
+        self.root = FinmarsDirectory.objects.create(**kwargs)
+
+        kwargs = dict(path=f"/root/path_1")
+        self.dir_1 = FinmarsDirectory.objects.create(parent=self.root, **kwargs)
+
+        kwargs = dict(path=f"/root/path_2")
+        self.dir_2 = FinmarsDirectory.objects.create(parent=self.root, **kwargs)
+
+        kwargs = dict(path=f"/root/path_1/path_3")
+        self.dir_3 = FinmarsDirectory.objects.create(parent=self.dir_1, **kwargs)
+
+        kwargs = dict(path=f"/root/path_4")
+        self.dir_4 = FinmarsDirectory.objects.create(parent=self.root, **kwargs)
+
+    @BaseTestCase.cases(
+        ("read", AccessLevel.READ),
+        ("full", AccessLevel.FULL),
+    )
+    def test__tree_access_to_dir(self, access):
+        self.create_dir_tree()
+        get_or_create_storage_access_policy(self.root, self.member, access)
+
+        self.assertTrue(member_has_access(self.dir_4, self.member, access))
+
+    @BaseTestCase.cases(
+        ("read", AccessLevel.READ),
+        ("full", AccessLevel.FULL),
+    )
+    def test__tree_access_by_root_level_dir_4(self, access):
+        self.create_dir_tree()
+        get_or_create_storage_access_policy(self.root, self.member, access)
+
+        self.assertTrue(member_has_access(self.dir_3, self.member, access))
+
+    @BaseTestCase.cases(
+        ("read", AccessLevel.READ),
+        ("full", AccessLevel.FULL),
+    )
+    def test__tree_access_by_root_level_dir_2(self, access):
+        self.create_dir_tree()
+        get_or_create_storage_access_policy(self.root, self.member, access)
+
+        self.assertTrue(member_has_access(self.dir_3, self.member, access))
+
+    @BaseTestCase.cases(
+        ("read", AccessLevel.READ),
+        ("full", AccessLevel.FULL),
+    )
+    def test__tree_access_by_root_dir_3(self, access):
+        self.create_dir_tree()
+        get_or_create_storage_access_policy(self.root, self.member, access)
+
+        self.assertTrue(member_has_access(self.dir_3, self.member, access))
