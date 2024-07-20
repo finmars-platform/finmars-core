5c627df7f59d95e1dc8ee3fcfaf75ba89a219add
dp.sokolov <sokolovdp@gmail.com>
2024-07-20 13:21:57 +0300

    PLAT-657 update explorer_permission.py

--- poms/explorer/explorer_permission.py
+++ poms/explorer/explorer_permission.py
@@ -2,15 +2,21 @@ import logging
 
 from rest_framework.exceptions import PermissionDenied
 
-from poms.iam.access_policy import AccessPolicy
+from poms.iam.access_policy import AccessPolicy as FinmarsAccessPermission
 from poms.iam.utils import get_statements
 
-_l = logging.getLogger("poms.iam")
+_l = logging.getLogger("poms.explorer")
 
 
-class ExplorerAccessPermission(AccessPolicy):
-    def get_policy_statements(self, request, view=None):
+class ExplorerAccessPermission(FinmarsAccessPermission):
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_superuser:
+            return True
+
         if not request.user.member:
             raise PermissionDenied(f"User {request.user.username} has no member")
 
-        return get_statements(member=request.user.member)
+        if request.user.member and request.user.member.is_admin:
+            return True
+
+        # check storage path permission
