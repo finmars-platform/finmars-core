88b6a2376fbccd9810ae0c66b09d231297b7f376
dp.sokolov <sokolovdp@gmail.com>
2024-08-28 18:53:42 +0300

    PLAT-845 develop directory into unified storage object

--- poms/explorer/models.py
+++ poms/explorer/models.py
@@ -36,6 +36,62 @@ class AccessLevel:
             )
 
 
+class FinmarsDirectory(MPTTModel, TimeStampedModel):
+    """
+    Model represents a directory in the Finmars storage (File system, AWS, Azure...).
+    """
+
+    path = models.CharField(
+        max_length=MAX_PATH_LENGTH,
+        unique=True,
+        blank=False,
+        help_text="Path to the directory in the storage system",
+    )
+    parent = TreeForeignKey(
+        "self",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="children",
+    )
+    size = models.PositiveBigIntegerField(
+        default=0,
+        help_text="Size of the file in bytes",
+    )
+    is_file = models.BooleanField(
+        default=False,
+        help_text="Is this directory a file",
+    )
+
+    class Meta:
+        ordering = ["path"]
+
+    class MPTTMeta:
+        level_attr = "mptt_level"
+        order_insertion_by = ["path"]
+
+    def __str__(self):
+        return self.path
+
+    def policy_user_code(self, access: str = AccessLevel.READ) -> str:
+        AccessLevel.validate_level(access)
+        return (
+            f"{get_default_configuration_code()}:{settings.SERVICE_NAME}"
+            f":explorer:{self.path}-{access}"
+        )
+
+    @property
+    def name(self) -> str:
+        path = Path(self.path)
+        return path.name
+
+    @property
+    def extension(self) -> str:
+        path = Path(self.path)
+        return path.suffix
+
+
+# DEPRECATED
 class ObjMixin:
     def __str__(self):
         return self.path
@@ -58,37 +114,7 @@ class ObjMixin:
         return path.suffix
 
 
-class FinmarsDirectory(MPTTModel, ObjMixin, TimeStampedModel):
-    """
-    Model represents a directory in the Finmars storage (File system, AWS, Azure...).
-    """
-
-    path = models.CharField(
-        max_length=MAX_PATH_LENGTH,
-        unique=True,
-        blank=False,
-        help_text="Path to the directory in the storage system",
-    )
-    parent = TreeForeignKey(
-        "self",
-        on_delete=models.CASCADE,
-        null=True,
-        blank=True,
-        related_name="children",
-    )
-
-    class Meta:
-        ordering = ["path"]
-
-    class MPTTMeta:
-        level_attr = "mptt_level"
-        order_insertion_by = ["path"]
-
-    @property
-    def size(self):
-        return 0
-
-
+# DEPRECATED
 class FinmarsFile(ObjMixin, TimeStampedModel):
     """
     Model represents a file in the Finmars storage (File system, AWS, Azure...).
