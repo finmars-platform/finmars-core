c343ab5e972cb85662fe08b1ec67ecb08d9df396
dp.sokolov <sokolovdp@gmail.com>
2024-09-26 22:08:12 +0300

    PLAT-845 add tests

--- poms/iam/tests/test_resource_group_model.py
+++ poms/iam/tests/test_resource_group_model.py
@@ -1,6 +1,7 @@
 from poms.common.common_base_test import BaseTestCase
 from poms.iam.models import ResourceGroup, ResourceGroupAssignment
 from poms.portfolios.models import Portfolio
+from django.core.exceptions import ObjectDoesNotExist
 
 
 class ResourceGroupViewTest(BaseTestCase):
@@ -61,3 +62,42 @@ class ResourceGroupViewTest(BaseTestCase):
         self.assertEqual(rg.assignments.first().resource_group, rg)
         ass = ResourceGroupAssignment.objects.filter(resource_group=rg).first()
         self.assertEqual(ass, rg.assignments.first())
+
+    def test_add_object_error_invalid_model(self):
+        rg = self.create_group()
+        portfolio = Portfolio.objects.first()
+
+        with self.assertRaises(ObjectDoesNotExist):
+            ResourceGroup.objects.add_object(
+                group_user_code=rg.user_code,
+                app_name="portfolios",
+                model_name="InvalidModel",
+                object_id=portfolio.id,
+                object_user_code=portfolio.user_code,
+            )
+
+    def test_add_object_error_invalid_app(self):
+        rg = self.create_group()
+        portfolio = Portfolio.objects.first()
+
+        with self.assertRaises(ObjectDoesNotExist):
+            ResourceGroup.objects.add_object(
+                group_user_code=rg.user_code,
+                app_name="invalid",
+                model_name="Portfolio",
+                object_id=portfolio.id,
+                object_user_code=portfolio.user_code,
+            )
+
+    def test_add_object_error_invalid_object_id(self):
+        rg = self.create_group()
+        portfolio = Portfolio.objects.first()
+
+        with self.assertRaises(ObjectDoesNotExist):
+            ResourceGroup.objects.add_object(
+                group_user_code=rg.user_code,
+                app_name="portfolios",
+                model_name="Portfolio",
+                object_id=self.random_int(10000, 1000000),
+                object_user_code=portfolio.user_code,
+            )
