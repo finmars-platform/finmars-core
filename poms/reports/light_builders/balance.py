3cfb8584a8443d7b4068e210af9fae8ade70b106
dp.sokolov <sokolovdp@gmail.com>
2024-10-30 20:33:35 +0300

    PLAT-1201 fix linters

--- poms/reports/light_builders/balance.py
+++ poms/reports/light_builders/balance.py
@@ -1,44 +1,13 @@
 import logging
-import os
 import time
-from datetime import timedelta
-
-from django.conf import settings
-from django.db import connection
 
 from celery import group
 
-from poms.accounts.models import Account, AccountType
-from poms.celery_tasks import finmars_task
+from poms.accounts.models import Account
 from poms.celery_tasks.models import CeleryTask
-from poms.common.utils import get_last_business_day
-from poms.currencies.models import Currency
 from poms.iam.utils import get_allowed_queryset
-from poms.instruments.models import (
-    Country,
-    ExposureCalculationModel,
-    Instrument,
-    InstrumentType,
-    LongUnderlyingExposure,
-    ShortUnderlyingExposure,
-)
 from poms.portfolios.models import Portfolio
 from poms.reports.common import Report
-from poms.reports.models import BalanceReportCustomField, ReportInstanceModel
-from poms.reports.sql_builders.helpers import (
-    dictfetchall,
-    get_cash_as_position_consolidation_for_select,
-    get_cash_consolidation_for_select,
-    get_fx_trades_and_fx_variations_transaction_filter_sql_string,
-    get_pl_left_join_consolidation,
-    get_position_consolidation_for_select,
-    get_report_fx_rate,
-    get_transaction_date_filter_for_initial_position_sql_string,
-    get_transaction_filter_sql_string,
-    get_where_expression_for_position_consolidation,
-)
-from poms.reports.sql_builders.pl import PLReportBuilderSql
-from poms.strategies.models import Strategy1, Strategy2, Strategy3
 from poms.users.models import EcosystemDefault
 
 _l = logging.getLogger("poms.reports")
@@ -188,10 +157,16 @@ class BalanceReportLightBuilderSql:
         _l.debug("Going to run %s tasks" % len(tasks))
 
         # Run the group of tasks
-        job = group(build.s(task_id=task.id, context={
-            "realm_code": self.instance.master_user.realm_code,
-            "space_code": self.instance.master_user.space_code
-        }) for task in tasks)
+        job = group(
+            build.s(
+                task_id=task.id,
+                context={
+                    "realm_code": self.instance.master_user.realm_code,
+                    "space_code": self.instance.master_user.space_code,
+                },
+            )
+            for task in tasks
+        )
 
         group_result = job.apply_async()
         # Wait for all tasks to finish and get their results
