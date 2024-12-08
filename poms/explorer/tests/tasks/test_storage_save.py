dbeca9302f7e3e34135f03e582f08f6d246d77fd
dp.sokolov <sokolovdp@gmail.com>
2024-12-08 22:36:42 +0300

    PLAT-870 fixing tests

--- poms/explorer/tests/tasks/test_storage_save.py
+++ poms/explorer/tests/tasks/test_storage_save.py
@@ -7,12 +7,17 @@ from poms.common.storage import FinmarsS3Storage
 class StorageSaveTest(BaseTestCase):
     def setUp(self):
         super().setUp()
-        self.init_test_case()
-        self.storage = FinmarsS3Storage()
 
-    @mock.patch("FinmarsS3Storage.save")
+    @mock.patch("poms.common.storage.FinmarsStorageMixin.save")
     @mock.patch("poms.explorer.tasks.start_update_create_path_in_storage")
-    def test__created_new_object(self, super_save, start_task):
-        self.storage.save(path="file.txt", content="test")
-        super_save.assert_called_once_with(path="file.txt", contents="test")
-        start_task.assert_called_once_with(path="file.txt", contents="test")
+    def test__two_saves_called(self, start_task, super_save):
+        FinmarsS3Storage().save(path="file.txt", content="test")
+        super_save.assert_called_once_with(path="file.txt", content="test")
+        start_task.assert_called_once_with(path="file.txt", content="test")
+
+    @mock.patch("poms.common.storage.FinmarsStorageMixin.save")
+    @mock.patch("poms.explorer.tasks.start_update_create_path_in_storage")
+    def test__one_save_called(self, super_save, start_task):
+        self.storage.save(path="check/.init", content=None)
+        super_save.assert_called_once_with(path="check/.init", content=None)
+        start_task.assert_not_called()
