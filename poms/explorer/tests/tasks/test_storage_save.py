dc4c27ce60f8c9a5f7710bf070c698ca6bbd23df
dp.sokolov <sokolovdp@gmail.com>
2024-12-08 22:48:19 +0300

    PLAT-870 fix & add tests

--- poms/explorer/tests/tasks/test_storage_save.py
+++ poms/explorer/tests/tasks/test_storage_save.py
@@ -5,19 +5,24 @@ from poms.common.storage import FinmarsS3Storage
 
 
 class StorageSaveTest(BaseTestCase):
-    def setUp(self):
-        super().setUp()
 
     @mock.patch("poms.common.storage.FinmarsStorageMixin.save")
     @mock.patch("poms.explorer.tasks.start_update_create_path_in_storage")
-    def test__two_saves_called(self, start_task, super_save):
+    def test__file_two_saves_called(self, start_task, super_save):
         FinmarsS3Storage().save(path="file.txt", content="test")
-        super_save.assert_called_once_with(path="file.txt", content="test")
-        start_task.assert_called_once_with(path="file.txt", content="test")
+        super_save.assert_called_once_with("file.txt", "test")
+        start_task.assert_called_once_with("file.txt", 4)
 
     @mock.patch("poms.common.storage.FinmarsStorageMixin.save")
     @mock.patch("poms.explorer.tasks.start_update_create_path_in_storage")
-    def test__one_save_called(self, super_save, start_task):
-        self.storage.save(path="check/.init", content=None)
-        super_save.assert_called_once_with(path="check/.init", content=None)
+    def test__dir_two_saves_called(self, start_task, super_save):
+        FinmarsS3Storage().save(path="dir", content=None)
+        super_save.assert_called_once_with("dir", None)
+        start_task.assert_called_once_with("dir", 0)
+
+    @mock.patch("poms.common.storage.FinmarsStorageMixin.save")
+    @mock.patch("poms.explorer.tasks.start_update_create_path_in_storage")
+    def test__one_save_called(self, start_task, super_save):
+        FinmarsS3Storage().save(path="check/.init", content=None)
+        super_save.assert_called_once_with("check/.init", None)
         start_task.assert_not_called()
