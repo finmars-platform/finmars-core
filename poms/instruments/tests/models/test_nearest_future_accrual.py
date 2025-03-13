a08dbef0505fa8e0519abd47068b55e95269e0c2
dp.sokolov <sokolovdp@gmail.com>
2025-03-13 13:19:26 +0300

    PLAT-1695 update test

--- poms/instruments/tests/models/test_nearest_future_accrual.py
+++ poms/instruments/tests/models/test_nearest_future_accrual.py
@@ -1,14 +1,12 @@
 from datetime import date
 
 from poms.common.common_base_test import BaseTestCase
-from poms.common.factories import AccrualFactory, AccrualCalculationModelFactory
-from poms.instruments.models import (
-    AccrualCalculationModel,
-    Accrual,
-    Instrument,
-)
+from poms.common.factories import AccrualFactory
+from poms.instruments.models import Instrument
 
+YEAR = BaseTestCase.today().year
 
+AMOUNT = 5
 
 class NearestFutureAccrualTest(BaseTestCase):
     databases = "__all__"
@@ -17,11 +15,29 @@ class NearestFutureAccrualTest(BaseTestCase):
         super().setUp()
         self.init_test_case()
         self.instrument = Instrument.objects.first()
-        self.start_year = self.today().year
 
     def create_accruals(self, amount: int) -> None:
-        for year in range(self.start_year, self.start_year + amount):
+        for year in range(YEAR, YEAR + amount):
             AccrualFactory(instrument=self.instrument, date=date(year=year, month=1, day=1))
 
-    def test__in_list(self):
-        self.create_accruals(5)
+    @BaseTestCase.cases(
+        ("0", date(year=YEAR+0, month=2, day=1)),
+        ("1", date(year=YEAR+1, month=2, day=1)),
+        ("2", date(year=YEAR+2, month=2, day=1)),
+        ("3", date(year=YEAR+3, month=2, day=1)),
+    )
+    def test__inside_list(self, target_date):
+        self.create_accruals(AMOUNT)
+        accrual = self.instrument.find_nearest_future_accrual(target_date)
+        self.assertIsNotNone(accrual)
+
+    @BaseTestCase.cases(
+        ("0", date(year=YEAR-1, month=2, day=1)),
+        ("1", date(year=YEAR-1, month=12, day=31)),
+        ("2", date(year=YEAR+7, month=2, day=1)),
+        ("3", date(year=YEAR+7, month=2, day=1)),
+    )
+    def test__outside_list(self, target_date):
+        self.create_accruals(AMOUNT)
+        accrual = self.instrument.find_nearest_future_accrual(target_date)
+        self.assertIsNone(accrual)
