cb2d286fdc31309b34beeeb94ce3cb2b4b016ab5
dp.sokolov <sokolovdp@gmail.com>
2025-03-15 13:59:03 +0300

    PLAT-1695 add tests

--- poms/instruments/tests/models/test_calculate_accrual_event_factor.py
+++ poms/instruments/tests/models/test_calculate_accrual_event_factor.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, timedelta
 
 from poms.common.common_base_test import BaseTestCase
 from poms.common.factories import (
@@ -8,6 +8,8 @@ from poms.common.factories import (
 )
 from poms.common.formula_accruals import calculate_accrual_event_factor
 from poms.instruments.models import Accrual
+
+PERIOD_DAYS = 365
 
 
 class CalculateAccrualEventFactorTests(BaseTestCase):
@@ -21,6 +23,7 @@ class CalculateAccrualEventFactorTests(BaseTestCase):
             instrument=self.default_instrument,
             accrual_calculation_model=AccrualCalculationModelFactory(model_type=model_type),
             date=day,
+            periodicity_n=PERIOD_DAYS,
         )
 
     @BaseTestCase.cases(
@@ -81,11 +84,12 @@ class CalculateAccrualEventFactorTests(BaseTestCase):
         ("DAY_COUNT_30_365", AccrualCalculationModel.DAY_COUNT_30_365),
         ("DAY_COUNT_SIMPLE", AccrualCalculationModel.DAY_COUNT_SIMPLE),
     )
-    def test_calculate_accrual_event_factor_act_360(self, model_type):
-        accrual_event = self.create_accrual_event(model_type, date(2026, 1, 1))
+    def test_calculate_accrual_event_factor_june_1st(self, model_type):
+        accrual_event = self.create_accrual_event(model_type=model_type, day=date(2026, 1, 1))
         accrual_factor = calculate_accrual_event_factor(accrual_event, date(2025, 6, 1))
 
-        print(f"June 1st {accrual_factor=}")
+        self.assertLessEqual(accrual_factor, 0.43)
+        self.assertGreaterEqual(accrual_factor, 0.404)
 
     @BaseTestCase.cases(
         ("DAY_COUNT_ACT_ACT_ICMA", AccrualCalculationModel.DAY_COUNT_ACT_ACT_ICMA),
@@ -110,8 +114,69 @@ class CalculateAccrualEventFactorTests(BaseTestCase):
         ("DAY_COUNT_30_365", AccrualCalculationModel.DAY_COUNT_30_365),
         ("DAY_COUNT_SIMPLE", AccrualCalculationModel.DAY_COUNT_SIMPLE),
     )
-    def test_calculate_accrual_event_factor_act_360(self, model_type):
-        accrual_event = self.create_accrual_event(model_type, date(2026, 1, 1))
+    def test_calculate_accrual_event_factor_december_31(self, model_type):
+        accrual_event = self.create_accrual_event(model_type=model_type, day=date(2026, 1, 1))
         accrual_factor = calculate_accrual_event_factor(accrual_event, date(2025, 12, 31))
 
-        print(f"December 31st {accrual_factor=}")
+        self.assertLessEqual(accrual_factor, 1.0)
+        self.assertGreaterEqual(accrual_factor, 0.989)
+
+    @BaseTestCase.cases(
+        ("DAY_COUNT_ACT_ACT_ICMA", AccrualCalculationModel.DAY_COUNT_ACT_ACT_ICMA),
+        ("DAY_COUNT_ACT_ACT_ISDA", AccrualCalculationModel.DAY_COUNT_ACT_ACT_ISDA),
+        ("DAY_COUNT_ACT_360", AccrualCalculationModel.DAY_COUNT_ACT_360),
+        ("DAY_COUNT_ACT_365L", AccrualCalculationModel.DAY_COUNT_ACT_365L),
+        ("DAY_COUNT_30_360_ISDA", AccrualCalculationModel.DAY_COUNT_30_360_ISDA),
+        ("DAY_COUNT_30E_PLUS_360", AccrualCalculationModel.DAY_COUNT_30E_PLUS_360),
+        ("DAY_COUNT_NL_365", AccrualCalculationModel.DAY_COUNT_NL_365),
+        ("DAY_COUNT_30_360_US", AccrualCalculationModel.DAY_COUNT_30_360_US),
+        ("DAY_COUNT_BD_252", AccrualCalculationModel.DAY_COUNT_BD_252),
+        ("DAY_COUNT_30_360_GERMAN", AccrualCalculationModel.DAY_COUNT_30_360_GERMAN),
+        ("DAY_COUNT_ACT_365_FIXED", AccrualCalculationModel.DAY_COUNT_ACT_365_FIXED),
+        ("DAY_COUNT_30E_360", AccrualCalculationModel.DAY_COUNT_30E_360),
+        ("DAY_COUNT_ACT_365A", AccrualCalculationModel.DAY_COUNT_ACT_365A),
+        ("DAY_COUNT_ACT_366", AccrualCalculationModel.DAY_COUNT_ACT_366),
+        ("DAY_COUNT_ACT_364", AccrualCalculationModel.DAY_COUNT_ACT_364),
+        # CURRENTLY UNUSED BY CBOND
+        ("DAY_COUNT_ACT_365", AccrualCalculationModel.DAY_COUNT_ACT_365),
+        ("DAY_COUNT_30_360_ISMA", AccrualCalculationModel.DAY_COUNT_30_360_ISMA),
+        ("DAY_COUNT_ACT_ACT_AFB", AccrualCalculationModel.DAY_COUNT_ACT_ACT_AFB),
+        ("DAY_COUNT_30_365", AccrualCalculationModel.DAY_COUNT_30_365),
+        ("DAY_COUNT_SIMPLE", AccrualCalculationModel.DAY_COUNT_SIMPLE),
+    )
+    def test_calculate_accrual_event_factor_january_1(self, model_type):
+        accrual_event = self.create_accrual_event(model_type=model_type, day=date(2026, 1, 1))
+        accrual_factor = calculate_accrual_event_factor(accrual_event, date(2026, 1, 1))
+
+        self.assertEqual(accrual_factor, 1.0)
+
+    @BaseTestCase.cases(
+        ("DAY_COUNT_ACT_ACT_ICMA", AccrualCalculationModel.DAY_COUNT_ACT_ACT_ICMA),
+        ("DAY_COUNT_ACT_ACT_ISDA", AccrualCalculationModel.DAY_COUNT_ACT_ACT_ISDA),
+        ("DAY_COUNT_ACT_360", AccrualCalculationModel.DAY_COUNT_ACT_360),
+        ("DAY_COUNT_ACT_365L", AccrualCalculationModel.DAY_COUNT_ACT_365L),
+        ("DAY_COUNT_30_360_ISDA", AccrualCalculationModel.DAY_COUNT_30_360_ISDA),
+        ("DAY_COUNT_30E_PLUS_360", AccrualCalculationModel.DAY_COUNT_30E_PLUS_360),
+        ("DAY_COUNT_NL_365", AccrualCalculationModel.DAY_COUNT_NL_365),
+        ("DAY_COUNT_30_360_US", AccrualCalculationModel.DAY_COUNT_30_360_US),
+        ("DAY_COUNT_BD_252", AccrualCalculationModel.DAY_COUNT_BD_252),
+        ("DAY_COUNT_30_360_GERMAN", AccrualCalculationModel.DAY_COUNT_30_360_GERMAN),
+        ("DAY_COUNT_ACT_365_FIXED", AccrualCalculationModel.DAY_COUNT_ACT_365_FIXED),
+        ("DAY_COUNT_30E_360", AccrualCalculationModel.DAY_COUNT_30E_360),
+        ("DAY_COUNT_ACT_365A", AccrualCalculationModel.DAY_COUNT_ACT_365A),
+        ("DAY_COUNT_ACT_366", AccrualCalculationModel.DAY_COUNT_ACT_366),
+        ("DAY_COUNT_ACT_364", AccrualCalculationModel.DAY_COUNT_ACT_364),
+        # CURRENTLY UNUSED BY CBOND
+        ("DAY_COUNT_ACT_365", AccrualCalculationModel.DAY_COUNT_ACT_365),
+        ("DAY_COUNT_30_360_ISMA", AccrualCalculationModel.DAY_COUNT_30_360_ISMA),
+        ("DAY_COUNT_ACT_ACT_AFB", AccrualCalculationModel.DAY_COUNT_ACT_ACT_AFB),
+        ("DAY_COUNT_30_365", AccrualCalculationModel.DAY_COUNT_30_365),
+        ("DAY_COUNT_SIMPLE", AccrualCalculationModel.DAY_COUNT_SIMPLE),
+    )
+    def test_calculate_accrual_event_factor_one_year_ago(self, model_type):
+        accrual_event = self.create_accrual_event(model_type=model_type, day=date(2026, 1, 1))
+
+        price_date = date(2026, 1, 1) - timedelta(days=PERIOD_DAYS)
+        accrual_factor = calculate_accrual_event_factor(accrual_event, price_date)
+
+        self.assertEqual(accrual_factor, 0.0)
