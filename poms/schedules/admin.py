aee39fbda2debd2266f3c284837ed7824fc90bf5
dp.sokolov <sokolovdp@gmail.com>
2025-03-17 11:53:48 +0300

    PLAT-1734 refac

--- poms/schedules/admin.py
+++ poms/schedules/admin.py
@@ -5,19 +5,38 @@ from poms.schedules.models import Schedule, ScheduleInstance, ScheduleProcedure
 
 class ScheduleProcedureInline(admin.TabularInline):
     model = ScheduleProcedure
-    fields = ["id", "type", "user_code"]
+    fields = [
+        "id",
+        "type",
+        "user_code",
+    ]
 
 
 class ScheduleAdmin(admin.ModelAdmin):
     model = Schedule
-    list_display = ["id", "master_user", "name", "user_code", "cron_expr", "next_run_at", "last_run_at"]
+    list_display = [
+        "id",
+        "master_user",
+        "name",
+        "user_code",
+        "cron_expr",
+        "next_run_at",
+        "last_run_at",
+    ]
     raw_id_fields = ["master_user"]
     inlines = [ScheduleProcedureInline]
 
 
 class ScheduleInstanceAdmin(admin.ModelAdmin):
     model = ScheduleInstance
-    list_display = ["id", "master_user", "schedule", "status", "created_at", "current_processing_procedure_number"]
+    list_display = [
+        "id",
+        "master_user",
+        "schedule",
+        "status",
+        "created_at",
+        "current_processing_procedure_number",
+    ]
     raw_id_fields = ["master_user", "schedule"]
 
 
