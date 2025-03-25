45bef8d19c9874959d1f095ce5939c11ca166b03
dp.sokolov <sokolovdp@gmail.com>
2025-03-25 18:50:37 +0300

    PLAT-1704 update tests

--- poms/celery_tasks/tests/test_list_all_tasks_view.py
+++ poms/celery_tasks/tests/test_list_all_tasks_view.py
@@ -9,5 +9,19 @@ class CeleryTaskViewSetTest(BaseTestCase):
         self.init_test_case()
         self.url = f"/{self.realm_code}/{self.space_code}/api/v1/tasks/task/list-all/"
 
-    def test__list_all_71(self):
-        pass
+    @BaseTestCase.cases(
+        ("1", "configuration.import_configuration"),
+        ("2", "csv_import.simple_import"),
+        ("3", "explorer.tasks.move_directory_in_storage"),
+        ("5", "instruments.calculate_prices_accrued_price"),
+        ("6", "portfolios.calculate_portfolio_register_record"),
+        ("7", "reconciliation.process_bank_file_for_reconcile"),
+    )
+    def test__list_all_71(self, task_name):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200, response.content)
+
+        response_json = response.json()
+
+        self.assertEqual(len(response_json), 70)
+        self.assertIn(task_name, set(response_json))
